423c27ecb5493ab12171f10d2c0d5a5c69b83f96
Eng-Ali Ghanme <Abed703105@gmail.com>
2022-02-11 21:00:23 +0200

    Project

---
+++ app/src/main/java/com/example/diabestes_care_app/Home_Activitey.java
@@ -0,0 +1,14 @@
+package com.example.diabestes_care_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home_Activitey extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_patient);
+    }
+}
---
+++ app/src/main/java/com/example/diabestes_care_app/Patient_sing_up.java
@@ -0,0 +1,14 @@
+package com.example.diabestes_care_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Patient_sing_up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_sing_up);
+    }
+}

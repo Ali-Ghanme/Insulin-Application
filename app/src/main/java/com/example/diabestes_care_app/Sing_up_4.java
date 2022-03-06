425b6666621a367fe7e6013f33b5e9d8cc28d7a7
Eng-Ali Ghanme <Abed703105@gmail.com>
2022-03-06 21:17:54 +0200

    Update For All :) Ahmed Zwaied Mohammed Siam Abdallah Al Dali

---
+++ app/src/main/java/com/example/diabestes_care_app/Sing_up_4.java
@@ -0,0 +1,14 @@
+package com.example.diabestes_care_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sing_up_4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up4);
+    }
+}
---
+++ app/src/main/java/com/example/diabestes_care_app/Sing_up_5_final.java
@@ -0,0 +1,14 @@
+package com.example.diabestes_care_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sing_up_5_final extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up5_final);
+    }
+}

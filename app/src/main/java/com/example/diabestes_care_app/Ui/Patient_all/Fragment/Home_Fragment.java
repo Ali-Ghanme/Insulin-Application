75411972601fa1d38292d070174a31995b865a68
Eng-Ali Ghanme <Abed703105@gmail.com>
2022-04-17 00:17:04 +0300

    Fix Error

---
+++ app/src/main/java/com/example/diabestes_care_app/Ui/Patient_all/Fragment/Home_Fragment.java
@@ -0,0 +1,22 @@
+package com.example.diabestes_care_app.Ui.Patient_all.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.diabestes_care_app.R;
+
+public class Home_Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_, container, false);
+
+    }
+}
--- app/src/main/java/com/example/diabestes_care_app/Ui/Patient_all/Home_Patient.java
+++ app/src/main/java/com/example/diabestes_care_app/Ui/Patient_all/Home_Patient.java
@@ -1,13 +1,28 @@
 package com.example.diabestes_care_app.Ui.Patient_all;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.example.diabestes_care_app.R;
+import com.example.diabestes_care_app.Ui.Patient_all.Fragment.Care_Fragment;
+import com.example.diabestes_care_app.Ui.Patient_all.Fragment.Chat_Fragment;
+import com.example.diabestes_care_app.Ui.Patient_all.Fragment.Home_Fragment;
+import com.example.diabestes_care_app.Ui.Patient_all.Fragment.Profile_Fragment;
+
+import nl.joery.animatedbottombar.AnimatedBottomBar;
 
 public class Home_Patient extends AppCompatActivity {
+    private static final String TAG = Home_Patient.class.getSimpleName();
+    AnimatedBottomBar animatedBottomBar;
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +30,43 @@ public class Home_Patient extends AppCompatActivity {
         setContentView(R.layout.activity_home_p);
         // code  is to make the Activity full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        animatedBottomBar = findViewById(R.id.navigation);
+
+        if (savedInstanceState == null) {
+            animatedBottomBar.selectTabById(R.id.nav_menu_home, true);
+            fragmentManager = getSupportFragmentManager();
+            Home_Fragment home_fragment = new Home_Fragment();
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, home_fragment).commit();
+        }
+        animatedBottomBar.setOnTabSelectListener(new AnimatedBottomBar.OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int lastIndex, @Nullable AnimatedBottomBar.Tab lastTab, int newIndex, @NonNull AnimatedBottomBar.Tab newTab) {
+                Fragment fragment = null;
+                switch (newTab.getId()) {
+                    case R.id.nav_menu_home:
+                        fragment = new Home_Fragment();
+                        break;
+                    case R.id.nav_menu_my_heartbeat:
+                        fragment = new Care_Fragment();
+                        break;
+                    case R.id.nav_menu_chat_:
+                        fragment = new Chat_Fragment();
+                        break;
+                    case R.id.nav_menu_user_profile:
+                        fragment = new Profile_Fragment();
+                        break;
+                }
+                if (fragment != null) {
+                    fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                } else {
+                    Log.e(TAG, "Error in Creating Fragment");
+                }
+            }
+            @Override
+            public void onTabReselected(int i, @NonNull AnimatedBottomBar.Tab tab) {
+                Toast.makeText(Home_Patient.this,  " أنت بلفعل في واجهة "+ tab.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

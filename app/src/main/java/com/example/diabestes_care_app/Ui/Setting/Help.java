c18caa50f96e2763f4a563c6dd270eb002f57332
MohammedSiam0 <mohammedsiam159@gmail.com>
2022-04-23 17:29:01 +0300

    Created Setting Pages Dr Xml

---
+++ app/src/main/java/com/example/diabestes_care_app/Ui/Setting/Help.java
@@ -0,0 +1,14 @@
+package com.example.diabestes_care_app.Ui.Setting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.diabestes_care_app.R;
+public class Help extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}

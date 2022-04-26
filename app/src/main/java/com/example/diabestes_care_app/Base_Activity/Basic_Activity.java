3115c7c7d869ffe3314b09dcb23a1838282ed538
Eng-Ali Ghanme <Abed703105@gmail.com>
2022-04-27 02:24:44 +0300

    Add & Edit Sitting

--- app/src/main/java/com/example/diabestes_care_app/Base_Activity/Basic_Activity.java
+++ app/src/main/java/com/example/diabestes_care_app/Base_Activity/Basic_Activity.java
@@ -16,4 +16,32 @@ public class Basic_Activity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
+    public static boolean isValidEmail(CharSequence target) {
+        if (target == null)
+            return false;
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+
+    public boolean validCellPhone(String number) {
+        return android.util.Patterns.PHONE.matcher(number).matches();
+    }
+
+    public boolean validUserName(String userName) {
+        if (userName.startsWith("P")) {
+            return true;
+        } else return userName.length() <= 15;
+    }
+
+    public boolean validPassword(String password) {
+        return password.length() < 8;
+    }
+
+    public boolean validCoPassword(String password, String CoPassword) {
+        return password.equals(CoPassword);
+    }
+
+    public boolean validIsEmpty(String name, String username, String phone, String email, String password, String coPassword) {
+        return name.isEmpty() || username.isEmpty() || phone.isEmpty() || email.isEmpty() || password.isEmpty()
+                || coPassword.isEmpty();
+    }
 }
--- app/src/main/java/com/example/diabestes_care_app/Ui/Patient_all/Setting_P/Edit_Profile_P.java
+++ app/src/main/java/com/example/diabestes_care_app/Ui/Patient_all/Setting_P/Edit_Profile_P.java
@@ -1,35 +1,77 @@
 package com.example.diabestes_care_app.Ui.Patient_all.Setting_P;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.example.diabestes_care_app.Base_Activity.Basic_Activity;
 import com.example.diabestes_care_app.R;
+import com.example.diabestes_care_app.Ui.Patient_all.Setting_P.Edit_Profile_Fragmane.Diabats_Info;
+import com.example.diabestes_care_app.Ui.Patient_all.Setting_P.Edit_Profile_Fragmane.Personal_Info;
 import com.example.diabestes_care_app.Ui.Sing_In.Fragment.Doctor_Fragment;
 import com.example.diabestes_care_app.Ui.Sing_In.Fragment.Patient_Fragment;
 import com.example.diabestes_care_app.Ui.Sing_In.Sing_In;
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.ArrayList;
 
 public class Edit_Profile_P extends Basic_Activity {
     TabLayout tabLayout;
     ViewPager viewPager_subject;
-    Sing_In.MainAdapter adapter;
+    MainAdapter2 adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         fullscreen();
         setContentView(R.layout.activity_edit_profile_p);
+
         tabLayout = findViewById(R.id.tab_layout_2);
         viewPager_subject = findViewById(R.id.viewpager_tab_2);
 
-        adapter = new Sing_In.MainAdapter(getSupportFragmentManager());
-        adapter.AddFragment(new Patient_Fragment(), "مريض");
-        adapter.AddFragment(new Doctor_Fragment(), "طبيب");
+        adapter = new MainAdapter2(getSupportFragmentManager());
+        adapter.AddFragment(new Personal_Info(), "بيانات شخصية");
+        adapter.AddFragment(new Diabats_Info(), "بيانات مرضية");
 
         viewPager_subject.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager_subject);
+    }
+
+
+    public static class MainAdapter2 extends FragmentPagerAdapter {
+        ArrayList<Fragment> fragmentArrayList = new ArrayList<>();
+        ArrayList<String> stringArrayList = new ArrayList<>();
+
+        //Create Constructor
+        public void AddFragment(Fragment fragment, String s) {
+            fragmentArrayList.add(fragment);
+            stringArrayList.add(s);
+        }
+
+        public MainAdapter2(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentArrayList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentArrayList.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return stringArrayList.get(position);
+        }
     }
 }

b6c6048d729e1aa5727a6eaaf5a8d17204dec6da
MohammedSiam0 <mohammedsiam159@gmail.com>
2022-08-13 18:50:18 +0300

    Update All  - Mohammed Siam

--- app/src/main/java/com/example/diabestes_care_app/Ui/Patient_all/Sections/Reports/Graphs_Fragment.java
+++ app/src/main/java/com/example/diabestes_care_app/Ui/Patient_all/Sections/Reports/Graphs_Fragment.java
@@ -1,9 +1,9 @@
 package com.example.diabestes_care_app.Ui.Patient_all.Sections.Reports;
 
-import static android.content.Context.MODE_PRIVATE;
-
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.icu.text.DateFormat;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,7 +15,11 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.diabestes_care_app.Adapters.Reports_Adapter;
+import com.example.diabestes_care_app.Models.Reports_Model;
 import com.example.diabestes_care_app.R;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,22 +28,27 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.LegendRenderer;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.ArrayList;
+import java.util.List;
+import static android.content.Context.MODE_PRIVATE;
 
-public class Graphs_Fragment extends Fragment {
-    TextView mohammed; // example
+ public class Graphs_Fragment extends Fragment {
+
     // button week and day
     Button week, day;
     GraphView graph;
     String suger;
-    int   suger_string ;
+    int sugarInt, sugerindex0, sugerindex1, sugerindex2;
     // Firebase
     DatabaseReference databaseReference;
     String PatientUsername;
     public static final String MyPREFERENCES_P = "P_Username";
-    public int dataa1, dataa2;
+
 
     // رسوم بيانية
     @Override
@@ -54,49 +63,183 @@ public class Graphs_Fragment extends Fragment {
         PatientUsername = prefs.getString("TAG_NAME", null);
         //============================Configure Firebase============================================
         databaseReference = FirebaseDatabase.getInstance().getReference("patient").child(PatientUsername).child("Reports_info").child("فحص يومي");
-
+        List<String> friends = new ArrayList<>();
         Query query = databaseReference.orderByKey();
         query.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 try {
+
                     for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                           suger = snapshot.child("نسبة السكر في الدم").getValue().toString();
-                        Toast.makeText(getContext(), suger, Toast.LENGTH_SHORT).show();
+                        String friend = snapshot.child("نسبة السكر في الدم").getValue().toString();
+                        friends.add(friend);
+
                     }
                 } catch (Exception e) {
                     Toast.makeText(getContext(), ".00.", Toast.LENGTH_SHORT).show();
-
                 }
+//                sugarInt = Integer.parseInt(suger);
+//                sugerindex0 = Integer.parseInt(suger);
+//                sugerindex1 = Integer.parseInt(suger);
+//                sugerindex2 = Integer.parseInt(suger);
+//                sugerindex0 = suger.indexOf(0);
+//                sugerindex1 = suger.indexOf(1);
+//                sugerindex2 = suger.indexOf(2,0);
+                String listindexone =  friends.get(0);
+                String listindextow =  friends.get(1);
+                String listindexthree =  friends.get(2);
+                sugerindex0 = Integer.parseInt(listindexone);
+                sugerindex1 = Integer.parseInt(listindextow);
+                sugerindex2 = Integer.parseInt(listindexthree);
+                LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>();
 
+                if (suger != null) {
+
+
+                        series.appendData(new DataPoint(1, 50), true, 5);
+                        series.appendData(new DataPoint(2, sugerindex0), true, 5);
+                        series.appendData(new DataPoint(3, sugerindex1), true, 5);
+                        series.appendData(new DataPoint(4, sugerindex2), true, 5);
+                        series.appendData(new DataPoint(5, 80), true, 5);
+
+//                    series.appendData(new DataPoint(2, sugerindex0), false, 5);
+//                    series.appendData(new DataPoint(3, sugerindex1), false, 5);
+//                    series.appendData(new DataPoint(4, sugerindex2), false, 5);
+//                    series.appendData(new DataPoint(5, 70), true, 5);
+
+
+                    // Date currentTime = Calendar.getInstance().getTime();
+//                            series.appendData(new DataPoint(1, sugarInt), true, 5);
+//                            series.appendData(new DataPoint(2, sugarInt), true, 5);
+//                            series.appendData(new DataPoint(3, sugarInt), true, 5);
+//                            series.appendData(new DataPoint(4, sugarInt), true, 5);
+                    //تفاصيل عن تسمية النقطة
+                    // series.setColor(Color.rgb(0, 80, 100));
+                    series.setColor(Color.RED);
+                    series.setTitle("نسبة السكر");
+                    series.setDrawDataPoints(true);
+                    series.setDataPointsRadius(15);
+                    series.setThickness(2);
+                    graph.addSeries(series);
+                    //تسمية المحاور
+
+                    graph.getViewport().setYAxisBoundsManual(true);
+                    graph.getViewport().setMinY(0);
+                    graph.getViewport().setMaxY(250);
+
+
+                } else {
+                    Toast.makeText(getContext(), "لم يتحقق الشرط", Toast.LENGTH_SHORT).show();
+                }
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Log.e("TAG", error.getMessage());
             }
         });
 
-       // Date currentTime = Calendar.getInstance().getTime();
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>();
-        series.appendData(new DataPoint(0, 50), true, 5);
-        series.appendData(new DataPoint(1, 150), true, 5);
-        series.appendData(new DataPoint(2, 120), true, 5);
-        series.appendData(new DataPoint(3, 200), true, 5);
-        series.appendData(new DataPoint(4, 100), true, 5);
 
-         //تفاصيل عن تسمية النقطة
-        // series.setColor(Color.rgb(0, 80, 100));
+//        dataa1 = Integer.parseInt(Bold);
+//        dataa2 = Integer.parseInt(Time);
+
+/*
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                blood = snapshot.child("patient").child(P_Username).child("Reports_info").child("فحص يومي").child("نسبة السكر في الدم").getValue(String.class);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        int[] Data = {dataa2, dataa2};
+
+        //
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>();
+//        for (int i=0;i<90;i++) {
+//
+//            series.appendData(new DataPoint(i, 20), false, 90);
+//        }
+        series.appendData(new DataPoint(1, 70), false, 5 );
+        series.appendData(new DataPoint(2, 50), false, 5);
+        series.appendData(new DataPoint(3, 80), false, 5);
+        series.appendData(new DataPoint(4, 120), false, 5);
+        series.appendData(new DataPoint(5, 200), false, 5);
+        series.appendData(new DataPoint(6, 10), false, 5);
+
+//        series.appendData(new DataPoint(20, 60), true, 20);
+//        series.appendData(new DataPoint(40, 40), true, 20);
+//        series.appendData(new DataPoint(44, 55), true, 20);
+//        series.appendData(new DataPoint(2, 5), true, 20);
+
+        //  new DataPoint(50  , 60);
+
+
+        //تفاصيل عن تسمية النقطة
+       // series.setColor(Color.rgb(0, 80, 100));
         series.setColor(Color.RED);
         series.setTitle("نسبة السكر");
         series.setDrawDataPoints(true);
-        series.setDataPointsRadius(15);
-        series.setThickness(2);
-        graph.addSeries(series);
-        //تسمية المحاور
+        series.setDataPointsRadius(50);
+        series.setThickness(8);
 
+        // اضافة النقاط على ال graph
+        graph.addSeries(series);
+
+        //عنوان ال graph
+        graph.setTitle("مخطط السكر اليومي");
+        graph.setTitleTextSize(50);
+        graph.setTitleColor(Color.RED);
+        //اتجه مسمى النقطة على اي مكان
+        graph.getLegendRenderer().setVisible(true);
+        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.BOTTOM);
+
+        //تسمية المحاور
+        GridLabelRenderer gridLabel = graph.getGridLabelRenderer();
+     //   gridLabel.setHorizontalAxisTitle("X وقت الجرعات في اليوم  ");
+        gridLabel.setVerticalAxisTitle("Y نسبة السكر في كل وقت  ");
+
+
+        // activate horizontal zooming and scrolling
+        graph.getViewport().setScalable(false);
+        // activate horizontal scrolling
+        graph.getViewport().setScrollable(false);
+        // activate horizontal and vertical zooming and scrolling
+        graph.getViewport().setScalableY(false);
+        // activate vertical scrolling
+        graph.getViewport().setScrollableY(false);
+
+
+        // set manual X bounds
+//        graph.getViewport().setXAxisBoundsManual(false);
+//        graph.getViewport().setMinX(0);
+//        graph.getViewport().setMaxX(5);
+        // set manual Y bounds
         graph.getViewport().setYAxisBoundsManual(true);
-        graph.getViewport().setMinY(50);
+        graph.getViewport().setMinY(0);
         graph.getViewport().setMaxY(250);
+
+
+ */
+
+
+//
+//           if ((suger_string = Integer.parseInt(suger)) != 0) {
+//            for (int i = 0; i < 5; i++) {
+//
+//            }
+//         }else {
+//            Toast.makeText(getActivity(), "لا توجد نتيجة", Toast.LENGTH_SHORT).show();
+//
+//        }
+
+
         return view;
     }
 
